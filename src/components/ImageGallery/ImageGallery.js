e8a190f42c0899a472c07c8ea6c2cd1d6b68f1f4
LutsenkoVitaliy <lutsenko996@gmail.com>
2022-02-02 14:53:04 +0200

    fix

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -4,21 +4,32 @@ import { Component } from "react";
 import ImageGalleryItem from "./ImageGalleryItem";
 
 class ImageGallery extends Component {
-  state = {} 
+  state = {
+    pictures : null
+  } 
   
-  componentDidUpdate(prevProps) {
-    if (prevProps.pictureName !== this.props.pictureName)
-    console.log("Новое имя");
-    fetch(`https://pixabay.com/api/?q=${this.props.pictureName}&page=1&key=24021062-33a986e16cffce2cd7c29eb8f&image_type=photo&orientation=horizontal&per_page=12`)
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pictureName !== this.props.pictureName) {
+      console.log("Новое имя");
+      fetch(`https://pixabay.com/api/?q=${this.props.pictureName}&page=1&key=24021062-33a986e16cffce2cd7c29eb8f&image_type=photo&orientation=horizontal&per_page=12`)
+        .then(res => res.json())
+        .then(pictures => {
+          this.setState((prevState) => ({
+            this.state.page >
+          }))
+        })
+      
+    }
+   
   }
 
   render() { 
     return (
-      // <ul className="ImageGallery">
-        // {/* <ImageGalleryItem /> */}
-      /* </ul> */
-
       <>
+      {/* <ul className="ImageGallery">
+         {<ImageGalleryItem /> }
+       </ul>  */}
+      
       <p>{this.props.pictureName}</p>
       </>
     );
--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 
 export default function ImageGalleryItem (pictures, ) {
   return pictures.map(({ id, }) => 
-    <li key={id}class="gallery-item" >
+    <li key={id} class="gallery-item" >
         <img src="" alt="" />
       </li>
   );
